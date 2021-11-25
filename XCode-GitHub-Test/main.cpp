ec237835b773b0d6fea4c3f53c88a30498a56b6a
23vu1 <scottvu369@outlook.com>
2021-11-25 17:21:25 -0600

    Initial Commit

---
+++ XCode-GitHub-Test/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  XCode-GitHub-Test
+//
+//  Created by Scott Vu on 11/25/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
