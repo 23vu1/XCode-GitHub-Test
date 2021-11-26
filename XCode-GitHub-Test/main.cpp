f3022ee6deee0c8503726e0a4293310289dc55ba
23vu1 <scottvu369@outlook.com>
2021-11-25 18:30:27 -0600

    Update - 3

--- XCode-GitHub-Test/main.cpp
+++ XCode-GitHub-Test/main.cpp
@@ -11,6 +11,7 @@ int main(int argc, const char * argv[]) {
     // insert code here..
     std::cout << "Does this work properly?" << std::endl;
     std::cout << "Yes this does!" << std::endl;
-    std::cout << "Hello, World!\n";
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Does XCode Edit? " << std::endl;
     return 0;
 }
